6591e9b649de85939726feaa66e54a7065b0fe0e
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-04-23 14:13:44 -0300

    feat: get tweets

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express,{json} from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 console.log("Aplicação está on");
@@ -7,18 +7,28 @@ const app = express();
 app.listen(5000);
 app.use(cors()); app.use(json());
 
-let user = {username: "", avatar: ""};
-let tweet = {	username: "", avatar: ""};
+let user = { username: "", avatar: "" };
+let tweet = { username: "", avatar: "" };
 
-let usersArray = [];
-let tweetsArray = [];
+const usersArray = [];
+const tweetsArray = [];
 
-app.post("/sign-up", (req, res) =>{
+app.post("/sign-up", (req, res) => {
     const body = req.body;
-    user = {...user, username: body.username, avatar: body.avatar};
+    user = { ...user, username: body.username, avatar: body.avatar };
 
-    usersArray.push(user);
+    usersArray.unshift(user);
     console.log(usersArray);
     res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    if (tweetsArray.length <= 10) {
+        res.send(tweetsArray);
+    } else {
+        const tweetsToSend = tweetsArray.filter((tweet, index) => index <= 10);
+        res.send(tweetsToSend);
+    }
+    console.log(tweetsToSend);
 });
 
