62021adc6135dce44b6a229749d8a135abb2545b
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-04-23 14:00:26 -0300

    feat: sign up post request

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import express,{json} from "express";
 import cors from "cors";
 
+console.log("Aplicação está on");
+
 const app = express();
+app.listen(5000);
+app.use(cors()); app.use(json());
+
+let user = {username: "", avatar: ""};
+let tweet = {	username: "", avatar: ""};
+
+let usersArray = [];
+let tweetsArray = [];
+
+app.post("/sign-up", (req, res) =>{
+    const body = req.body;
+    user = {...user, username: body.username, avatar: body.avatar};
+
+    usersArray.push(user);
+    console.log(usersArray);
+    res.send("OK");
+});
+
